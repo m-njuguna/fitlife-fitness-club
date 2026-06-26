44fa64b8aab184a9a6a600acc4336e29f347829e
Mark Njuguna <markn5769@gmail.com>
2026-06-26 10:53:59 +0300

    Implement contact form validation

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("contactForm");
+
+if (form) {
+    form.addEventListener("submit", function (event) {
+
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const message = document.getElementById("message").value.trim();
+
+        if (name === "" || email === "" || message === "") {
+            alert("Please fill in all fields.");
+            event.preventDefault();
+        }
+    });
+}
